f87866ed1761d551aefe1a5ac511367f46700f99
Elijah wines <elijahwines84@yahoo.com>
2020-04-21 16:55:38 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,6 +10,12 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
-    
+    create_table :costumes do |c|
+      c.string :name 
+      c.integer :price 
+      c.string :size
+      c.string :image_url
+      c.timestamps 
+    end
   end
 end
